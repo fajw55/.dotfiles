4c9f5b6bcc0e01e50d132e4b73f8aa268662b345
Alan Jug <fajw55@gmail.com>
2025-07-14 14:21:30 +0200

    Wsl version

--- .config/nvim/lua/alan/core/options.lua
+++ .config/nvim/lua/alan/core/options.lua
@@ -1,17 +1,15 @@
 vim.cmd("let g:netrw_liststyle = 3")
 
-vim.g.clipboard = "wl-copy" -- use system clipboard as default register
-
 local opt = vim.opt -- for conciseness
 
 -- line numbers
 opt.relativenumber = true -- show relative line numbers
-opt.number = true -- shows absolute line number on cursor line (when relative number is on)
+opt.number = true         -- shows absolute line number on cursor line (when relative number is on)
 
 -- tabs & indentation
-opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
-opt.shiftwidth = 2 -- 2 spaces for indent width
-opt.expandtab = true -- expand tab to spaces
+opt.tabstop = 2       -- 2 spaces for tabs (prettier default)
+opt.shiftwidth = 2    -- 2 spaces for indent width
+opt.expandtab = true  -- expand tab to spaces
 opt.autoindent = true -- copy indent from current line when starting new one
 
 -- line wrapping
@@ -19,7 +17,7 @@ opt.wrap = false -- disable line wrapping
 
 -- search settings
 opt.ignorecase = true -- ignore case when searching
-opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
+opt.smartcase = true  -- if you include mixed case in your search, assumes you want case-sensitive
 
 -- cursor line
 opt.cursorline = true -- highlight the current cursor line
@@ -30,7 +28,7 @@ opt.cursorline = true -- highlight the current cursor line
 -- (have to use iterm2 or any other true color terminal)
 opt.termguicolors = true
 opt.background = "dark" -- colorschemes that can be light or dark will be made dark
-opt.signcolumn = "yes" -- show sign column so that text doesn't shift
+opt.signcolumn = "yes"  -- show sign column so that text doesn't shift
 
 -- backspace
 opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
@@ -43,8 +41,8 @@ opt.clipboard = "unnamedplus" -- use system clipboard as default register
 opt.splitright = true -- split vertical window to the right
 opt.splitbelow = true -- split horizontal window to the bottom
 
-opt.swapfile = false -- turn off swapfile
-opt.backup = false -- turn off backup file
+opt.swapfile = false  -- turn off swapfile
+opt.backup = false    -- turn off backup file
 
 -- gets rid of line with white spaces
 vim.g.editorconfig = true
