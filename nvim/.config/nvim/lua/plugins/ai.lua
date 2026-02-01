c328ca6ed128c7f918cb9865ef4efbe6eedc9abc
Alan Jug <fajw55@gmail.com>
2026-02-01 15:56:57 +0100

    Add opencode proper way

--- nvim/.config/nvim/lua/plugins/ai.lua
+++ nvim/.config/nvim/lua/plugins/ai.lua
@@ -2,18 +2,28 @@ return {
   -- NOTE: opencode
   {
     "sudo-tee/opencode.nvim",
-    dependencies = {
-      "nvim-lua/plenary",
-      "MufinTanjim/nui-vim",
-      "folke/snacks.nvim",
-    },
-    event = "VeryLazy",
     config = function()
-      require("opencode").setup({
-        opencode_executable = "opencode",
-        keymap_prefix = "<leader>o",
-      })
+      require("opencode").setup({})
     end,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      {
+        "MeanderingProgrammer/render-markdown.nvim",
+        opts = {
+          anti_conceal = { enabled = false },
+          file_types = { "markdown", "opencode_output" },
+        },
+        ft = { "markdown", "Avante", "copilot-chat", "opencode_output" },
+      },
+      -- Optional, for file mentions and commands completion, pick only one
+      "saghen/blink.cmp",
+      -- 'hrsh7th/nvim-cmp',
+      -- Optional, for file mentions picker, pick only one
+      "folke/snacks.nvim",
+      -- 'nvim-telescope/telescope.nvim',
+      -- 'ibhagwan/fzf-lua',
+      -- 'nvim_mini/mini.nvim',
+    },
   },
   -- NOTE: copilot (zbirenbaum)
   -- {
