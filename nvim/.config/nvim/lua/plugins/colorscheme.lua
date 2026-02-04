033ce2fa23477f7a912ae9bf5671248e7ea7142a
Alan Jug <fajw55@gmail.com>
2026-02-04 09:39:29 +0100

    Remove transparency from nvim

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -3,16 +3,16 @@ return {
     "folke/tokyonight.nvim",
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
-      local transparent = true
-
-      require("tokyonight").setup({
-        style = "night",
-        transparent = transparent,
-        styles = {
-          sidebars = transparent,
-          floats = transparent,
-        },
-      })
+      -- local transparent = true
+      --
+      -- require("tokyonight").setup({
+      --   style = "night",
+      --   transparent = transparent,
+      --   styles = {
+      --     sidebars = transparent,
+      --     floats = transparent,
+      --   },
+      -- })
       -- load the colorscheme here
       vim.cmd([[colorscheme tokyonight]])
     end,
