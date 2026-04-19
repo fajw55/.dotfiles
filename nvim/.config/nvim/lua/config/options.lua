7a2eedc893a511ba0f78916a63243dbeb5c73e74
Alan Jug <alan.jug@oriflame.com>
2026-04-19 13:22:20 +0200

    NVIM remove linux copy stuff

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -5,11 +5,7 @@
 -- vim.opt.scrolloff = 0 -- allow scrolloff
 vim.o.scroll = 5 -- for neoscroll
 
-vim.g.clipboard = "osc52"
-vim.opt.clipboard = ""
--- vim.opt.clipboard = "unnamedplus"
-
-vim.g.conform_format_on_save = { -- optional, override behavior
-  lsp_fallback = false,
-  timeout_ms = 500,
-}
+-- vim.g.conform_format_on_save = { -- optional, override behavior
+--   lsp_fallback = false,
+--   timeout_ms = 500,
+-- }
