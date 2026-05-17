ca21156ce7d5723755a08ef35e8c903acd3ed9bc
Alan Jug <fajw55@gmail.com>
2026-05-17 23:25:59 +0200

    NVIM - based on built in package manager

---
+++ nvim/.config/nvim/lua/current-theme.lua
@@ -0,0 +1,26 @@
+vim.pack.add({"https://github.com/folke/tokyonight.nvim"})
+
+vim.cmd.colorscheme("tokyonight-night")
+
+local function set_transparent() -- set UI component to transparent
+	local groups = {
+		"Normal",
+		"NormalNC",
+		"EndOfBuffer",
+		"NormalFloat",
+		"FloatBorder",
+		"SignColumn",
+		-- "StatusLine",
+		-- "StatusLineNC",
+		"TabLine",
+		"TabLineFill",
+		"TabLineSel",
+		"ColorColumn",
+	}
+	for _, g in ipairs(groups) do
+		vim.api.nvim_set_hl(0, g, { bg = "none" })
+	end
+	vim.api.nvim_set_hl(0, "TabLineFill", { bg = "none", fg = "#767676" })
+end
+
+set_transparent()
--- nvim/.config/nvim/lua/plugins/ts-autotag.lua
+++
@@ -1,7 +0,0 @@
-return {
-  {
-    "windwp/nvim-ts-autotag",
-    event = "VeryLazy",
-    config = true,
-  },
-}
